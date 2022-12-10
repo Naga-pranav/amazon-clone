ab5193013785a37e2baa0d67476a5f3b2cef2432
myname <pranavanaga@gmail.com>
2022-12-10 15:21:53 +0530

    nav push

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBEdxBoDtMtXU0Yom4pxg6SSkn08n5anZ0",
+  authDomain: "clone-adbab.firebaseapp.com",
+  projectId: "clone-adbab",
+  storageBucket: "clone-adbab.appspot.com",
+  messagingSenderId: "431239296506",
+  appId: "1:431239296506:web:bca5417cea6ee1ee4ce043",
+  measurementId: "G-ZX1L49XNZE"
+};
