487c57ac64508d021fa873b2e325229dea7c70f9
myname <pranavanaga@gmail.com>
2022-12-10 20:13:38 +0530

    route push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    
-    <div className="app">
-      
-      <Header />
-      <Home />
-
-
-
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/checkout" element={[<Header />, <h1>I am a checkout page</h1>]}/>
+          <Route path="/" element={[<Header />, <Home />]}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
